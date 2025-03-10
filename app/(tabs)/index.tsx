1a3c7ecc32d0e58fa4970c5189f98a55ce4ca3de
unknown <amirmon24@gmail.com>
2025-03-10 13:54:19 +0100

    fetch and display home page movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View,Image, ScrollView } from "react-native";
+import { Text, View,Image , ScrollView, ActivityIndicator, FlatList } from "react-native";
 import {images} from '@/constants/images'
 import {icons} from '@/constants/icons'
 import SearchBar from "@/components/SearchBar";
+import { useRouter } from 'expo-router'
+import useFetch from "@/services/UseFetch";
+import {fetchPopularMovies} from "@/services/api"
+
+import MovieCard from "@/components/MovieCard";
+
 
 export default function Index() {
+  const router = useRouter();
+
+  const {data: movies,loading:movieloading,error: movieError} = useFetch (()=>fetchPopularMovies({
+    query:''
+  }))
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -11,10 +23,45 @@ export default function Index() {
         minHeight:"100%",paddingBottom:10 }}>
       <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
+      {movieloading ? (
+        <ActivityIndicator 
+        size="large"
+        color="#0000ff"
+        className="mt-10 self-center"
+        />
+      ): movieError ? (
+        <Text>{movieError?.message}</Text>
+      ) : (
 <View className="flex-1 mt-5">
-<SearchBar/>
+<SearchBar 
+onPress={()=> router.push("/search")}
+placeholder="Search for a movie"
+/>
+<>
+<Text className="text-lg text-bold mt-5 mb-3 text-white">Latest Movies</Text>
 
+<FlatList 
+data={movies}
+renderItem={({item}) => (
+  <MovieCard { ...item}/>
+)}
+keyExtractor={(item) => item.id.toString()}
+numColumns={3}
+columnWrapperStyle= {{
+  justifyContent:"flex-start",
+  gap:20,
+  paddingRight : 5,
+  marginBottom: 10
+}}
+className="mt-2 pb-32"
+scrollEnabled={false}
+/>
+</>
 </View>
+      )
+    }
+
+
       </ScrollView>
     </View>
   );
